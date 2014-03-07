ffbf0ebc227ded560b5e518579817b68c726d7aa
Orakaro <nhatminh_179@hotmail.com>
2014-03-07 23:31:42 +0900

    init sample

---
+++ genConfig.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+	/* config file will be saved to $config_dir */
+	'config_dir' => 'config',
+	/* List of classes and static methods that you want to mock */
+	/* Key is Class name and Value is method */
+	'static_classes' => array(
+		'Book' => 'license',
+	),
+);
