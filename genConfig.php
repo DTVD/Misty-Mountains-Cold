4737e07e8e60a555833917faa39bf693873d0199
Orakaro <nhatminh_179@hotmail.com>
2014-03-07 23:35:19 +0900

    update genConfig

--- genConfig.php
+++ genConfig.php
@@ -1,11 +1,13 @@
 <?php
 
 return array(
-	/* config file will be saved to $config_dir */
-	'config_dir' => 'config',
-	/* List of classes and static methods that you want to mock */
-	/* Key is Class name and Value is method */
-	'static_classes' => array(
-		'Book' => 'license',
-	),
+  /* Bunch of files will be saved to $config_dir */
+  /* You will have to add a psr-4 define in your composer.json autoload section */
+  /* See example at "sample folder" */
+  'config_dir' => 'config/facade',
+  /* List of classes and static methods that you want to mock */
+  /* Key is class name and Value is method. Class can use with array of Methods */
+  'static_classes' => array(
+    'Book' => 'license',
+  ),
 );
